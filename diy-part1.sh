6deb79f4c6336aa75a24d57686633568bd3a371d
YokukYo <34328531+YokukYo@users.noreply.github.com>
2026-05-25 13:32:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-# 添加 dae feed
-echo 'src-git dae https://github.com/daeuniverse/openwrt-dae.git' >> feeds.conf.default
+# diy-part1.sh - 在 feeds update 之前执行
+
+# 克隆 openwrt-dae，复制 dae 包到编译树
+git clone --depth 1 https://github.com/daeuniverse/openwrt-dae.git /tmp/openwrt-dae
+cp -r /tmp/openwrt-dae/dae openwrt/package/dae
+cp -r /tmp/openwrt-dae/dae-geoip openwrt/package/dae-geoip
+cp -r /tmp/openwrt-dae/dae-geosite openwrt/package/dae-geosite
