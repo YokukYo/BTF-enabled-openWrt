9c9e0334470c23425bd0c84afe5749ce583f11f1
YokukYo <34328531+YokukYo@users.noreply.github.com>
2026-05-25 14:29:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
+# diy-part2.sh
+# 执行时 pwd 为 openwrt/
 
-cat >> .config << 'EOF'
-
-CONFIG_DEVEL=y
-
+cat >> .config << 'CONF'
 CONFIG_KERNEL_DEBUG_INFO=y
 CONFIG_KERNEL_DEBUG_INFO_BTF=y
 CONFIG_KERNEL_DEBUG_INFO_REDUCED=n
-
+CONFIG_KERNEL_CGROUP_BPF=y
 CONFIG_PACKAGE_dae=y
-
 CONFIG_PACKAGE_kmod-sched-bpf=y
+CONFIG_PACKAGE_kmod-sched-core=y
 CONFIG_PACKAGE_kmod-veth=y
 CONFIG_PACKAGE_kmod-xdp-sockets-diag=y
-
-EOF
+CONFIG_PACKAGE_kmod-nft-bridge=y
+CONF
