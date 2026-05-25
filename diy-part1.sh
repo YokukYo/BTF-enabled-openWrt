7cd87051063ded1a704ed48444aaa20fb0e5741c
YokukYo <34328531+YokukYo@users.noreply.github.com>
2026-05-25 14:13:21 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-# diy-part1.sh
-# 执行时 pwd 为 openwrt/
 
-wget -q https://github.com/daeuniverse/openwrt-dae/archive/refs/heads/main.zip -O /tmp/openwrt-dae.zip
-unzip -q /tmp/openwrt-dae.zip -d /tmp/
-mv /tmp/openwrt-dae-main /tmp/openwrt-dae
-
-cp -r /tmp/openwrt-dae/dae package/dae
-cp -r /tmp/openwrt-dae/dae-geoip package/dae-geoip
-cp -r /tmp/openwrt-dae/dae-geosite package/dae-geosite
+mkdir -p package/dae
